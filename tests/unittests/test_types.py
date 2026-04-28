c5db1c96a8a10428d3ec967bc1b2901c00e64a19
heffernane <275666952+heffernane@users.noreply.github.com>
2026-04-28 10:32:55 +0200

    Expand types.py tests

--- tests/unittests/test_types.py
+++ tests/unittests/test_types.py
@@ -3,25 +3,60 @@ from clinicaio.types import *
 import pytest
 from re import escape
 
-def test_ids():
+def test_ids_missing_prefix():
     with pytest.raises(BIDSException, match="BIDS subject ID 001 must start with sub-"):
         SubjectId("001")
     with pytest.raises(BIDSException, match="BIDS session ID M000 must start with ses-"):
         SessionId("M000")
-    
-    with pytest.raises(BIDSException, match="BIDS label can't be empty"):
+
+def test_ids_prefix_with_empty_label():
+    with pytest.raises(BIDSException, match=escape("BIDS subject id sub- had invalid label (in sub-<label>): BIDS label can't be empty")):
         SubjectId("sub-")
-    with pytest.raises(BIDSException, match="BIDS label can't be empty"):
+    with pytest.raises(BIDSException, match=escape("BIDS session id ses- had invalid label (in ses-<label>): BIDS label can't be empty")):
         SessionId("ses-")
 
+def test_ids_to_str():
     assert(SubjectId("sub-123").__str__() == "sub-123")
     assert(SessionId("ses-123").__str__() == "ses-123")
 
+def test_ids_non_alnum_chars():
     with pytest.raises(BIDSException, match=escape("BIDS subject id sub-é had invalid label (in sub-<label>): BIDS label é must be all [a-zA-Z0-9] characters")):
         SubjectId("sub-é")
     with pytest.raises(BIDSException, match=escape("BIDS session id ses-é had invalid label (in ses-<label>): BIDS label é must be all [a-zA-Z0-9] characters")):
         SessionId("ses-é")
 
+def test_ids_hash_eq():
+    sub1 = SubjectId("sub-01")
+    sub2 = SubjectId("sub-02")
+    sub1_same = SubjectId("sub-01")
+    # The label part of sub-<label> is not treated particularily with regards
+    # to fully integer labels.
+    sub1_same_but_different = SubjectId("sub-1")
+
+    assert(sub1.__hash__() == sub1_same.__hash__())
+    assert(sub1 == sub1_same)
+    assert(sub1.__hash__() != sub2.__hash__())
+    assert(sub1.__hash__() != sub1_same_but_different.__hash__())
+    assert(sub1 != sub1_same_but_different)
+    # In case __ne__() is broken
+    assert(not(sub1 == sub2))
+    assert(sub1 != sub2)
+
+    ses1 = SessionId("ses-01")
+    ses2 = SessionId("ses-02")
+    ses1_same = SessionId("ses-01")
+    # The label part of ses-<label> is not treated particularily with regards
+    # to fully integer labels.
+    ses1_same_but_different = SessionId("ses-1")
+
+    assert(ses1.__hash__() == ses1_same.__hash__())
+    assert(ses1 == ses1_same)
+    assert(ses1.__hash__() != ses2.__hash__())
+    assert(ses1.__hash__() != ses1_same_but_different.__hash__())
+    assert(ses1 != ses1_same_but_different)
+    # In case __ne__() is broken
+    assert(not(ses1 == ses2))
+    assert(ses1 != ses2)
 
 def test_enum_to_str():
     # We test this notably because while StrEnum has the correct behavior
