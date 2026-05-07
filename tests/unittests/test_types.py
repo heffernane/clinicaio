ed3418fab66245c5ef97f19b68ca8810b07a0f8e
heffernane <275666952+heffernane@users.noreply.github.com>
2026-05-07 13:41:42 +0200

    Make some tests parametric

--- tests/unittests/test_types.py
+++ tests/unittests/test_types.py
@@ -1,4 +1,5 @@
 from re import escape
+from typing import Any
 
 import pytest
 from packaging.version import Version
@@ -41,9 +42,15 @@ def test_ids_prefix_with_empty_label():
         SessionId("ses-")
 
 
-def test_ids_to_str():
-    assert str(SubjectId("sub-123")) == "sub-123"
-    assert str(SessionId("ses-123")) == "ses-123"
+@pytest.mark.parametrize(
+    ["id_class", "value"],
+    [
+        (SubjectId, "sub-123"),
+        (SessionId, "ses-123"),
+    ]
+)
+def test_ids_to_str(id_class: type, value: str):
+    assert str(id_class(value)) == value
 
 
 def test_ids_non_alnum_chars():
@@ -63,54 +70,55 @@ def test_ids_non_alnum_chars():
         SessionId("ses-é")
 
 
-def test_ids_hash_eq():
-    sub1 = SubjectId("sub-01")
-    sub2 = SubjectId("sub-02")
-    sub1_same = SubjectId("sub-01")
-    # The label part of sub-<label> is not treated particularily with regards
+@pytest.mark.parametrize(
+    ["id_class", "prefix"],
+    [
+        (SubjectId, "sub-"),
+        (SessionId, "ses-"),
+    ]
+)
+def test_ids_hash_eq(id_class: type, prefix: str):
+    id1 = id_class(f"{prefix}01")
+    id2 = id_class(f"{prefix}02")
+    id1_same = id_class(f"{prefix}01")
+    # The label part of <prefix><label> is not treated particularily with regards
     # to fully integer labels.
-    sub1_same_but_different = SubjectId("sub-1")
+    id1_same_but_different = id_class(f"{prefix}1")
 
-    assert sub1.__hash__() == sub1_same.__hash__()
-    assert sub1 == sub1_same
-    assert sub1.__hash__() != sub2.__hash__()
-    assert sub1.__hash__() != sub1_same_but_different.__hash__()
-    assert sub1 != sub1_same_but_different
+    assert id1.__hash__() == id1_same.__hash__()
+    assert id1 == id1_same
+    assert id1.__hash__() != id2.__hash__()
+    assert id1.__hash__() != id1_same_but_different.__hash__()
+    assert id1 != id1_same_but_different
     # In case __ne__() is broken
-    assert not (sub1 == sub2)
-    assert sub1 != sub2
-
-    ses1 = SessionId("ses-01")
-    ses2 = SessionId("ses-02")
-    ses1_same = SessionId("ses-01")
-    # The label part of ses-<label> is not treated particularily with regards
-    # to fully integer labels.
-    ses1_same_but_different = SessionId("ses-1")
-
-    assert ses1.__hash__() == ses1_same.__hash__()
-    assert ses1 == ses1_same
-    assert ses1.__hash__() != ses2.__hash__()
-    assert ses1.__hash__() != ses1_same_but_different.__hash__()
-    assert ses1 != ses1_same_but_different
-    # In case __ne__() is broken
-    assert not (ses1 == ses2)
-    assert ses1 != ses2
+    assert not (id1 == id2)
+    assert id1 != id2
 
 
-def test_enum_to_str():
+@pytest.mark.parametrize(
+    ["enum_value", "string_value"],
+    [
+        (BIDSDatasetType.RAW, "raw"),
+        (DataType.PHENOTYPE, "phenotype"),
+        (FileExtension.NII_GZ, "nii.gz"),
+    ]
+)
+def test_enum_to_str(enum_value: Any, string_value: str):
     # We test this notably because while StrEnum has the correct behavior
     # when we inherit from it, class Foo(str, Enum) does not (in some
     # Python versions). str(A.FOO) outputs A.FOO in Python 3.10 but f"{A.FOO}" outputs foo
     # (as f"..." goes through __format__() which seems to have a different implementation)
-    assert str(BIDSDatasetType.RAW) == "raw"
-    assert str(DataType.PHENOTYPE) == "phenotype"
-    assert str(FileExtension.NII_GZ) == "nii.gz"
-    assert f"{BIDSDatasetType.RAW}" == "raw"
-    assert f"{DataType.PHENOTYPE}" == "phenotype"
-    assert f"{FileExtension.NII_GZ}" == "nii.gz"
+    assert str(enum_value) == string_value
+    assert f"{enum_value}" == string_value
 
 
-def test_wrappers_to_str():
-    assert str(Suffix("sfx")) == "sfx"
-    assert str(Version("1.10.0")) == "1.10.0"
-    assert str(Label("txt")) == "txt"
+@pytest.mark.parametrize(
+    ["wrapper_type", "string"],
+    [
+        (Suffix, "sfx"),
+        (Version, "1.10.0"),
+        (Label, "txt")
+    ]
+)
+def test_wrappers_to_str(wrapper_type: type, string: str):
+    assert str(wrapper_type(string)) == string
