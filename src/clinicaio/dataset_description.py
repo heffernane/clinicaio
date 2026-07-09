9817f24eeea05d427a67b1d60df4161070aa288c
heffernane <275666952+heffernane@users.noreply.github.com>
2026-07-09 10:57:08 +0200

    Allow extra dataset description fields

--- src/clinicaio/dataset_description.py
+++ src/clinicaio/dataset_description.py
@@ -27,7 +27,7 @@ class BIDSDatasetDescription(BaseModel):
         validate_assignment=True,
         validate_default=True,
         arbitrary_types_allowed=True,
-        extra="ignore",
+        extra="allow",
         strict=True,
     )
 
